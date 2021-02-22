aa4cb6ca636dc0bdb019b5a4d98becbc24142147
Breno Xavier <brenoxavier1227@gmail.com>
2021-02-22 17:58:22 -0300

    Adding description in the type file #2

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,15 @@
 interface Content {
+  /** Page URL */
   loc: string
+  /** Last page change */
   lastmod?: string
+  /** List of parameters for images */
   images?: [{
+    /** Image URL */
     loc: string
+    /** Image title */
     title?: string
+    /** Image description */
     description?: string
   }]
 }
